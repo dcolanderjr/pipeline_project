5834e5935fa6acdea5a81a9465b61b750a27c8a2
dC <131455625+dcolanderjr@users.noreply.github.com>
2024-02-20 09:32:08 -0500

    clean up

--- backend-s3/backend.tf
+++
@@ -1,9 +0,0 @@
-# Backend for storing the state file in S3 bucket
-
-terraform {
-  backend "s3" {                            
-    bucket = "pipe-line-project"         
-    key    = "terraform.tfstate"           
-    region = "us-east-1"                 
-  }
-}
